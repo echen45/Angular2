5778c24b924e50df20cda61a45c4c4a23c4f30ec
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-01-05 19:07:14 -0500

    corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProfileComponent } from './views/profile/profile.component';
 import { LandingPageLoginComponent } from './views/landing-page-login/landing-page-login.component';
 import { LoginboxComponent } from './components/loginbox/loginbox.component';
 import { ProfileboxComponent } from './profilebox/profilebox.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ProfileboxComponent } from './profilebox/profilebox.component';
     ProfileComponent,
     LandingPageLoginComponent,
     LoginboxComponent,
-    ProfileboxComponent
+    ProfileboxComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
